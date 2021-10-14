83ffc190ee31ef78445458b42294fe03567b3733
Karanraj Chauhan <kachau@redhat.com>
2021-10-14 10:08:02 -0400

    Add cascadetabnet url and config file as in their offiical repo demo nb

--- src/components/utils/cscdtabnet_checkpoint_url.py
+++ src/components/utils/cscdtabnet_checkpoint_url.py
@@ -7,4 +7,6 @@ checkpoint_url = {
     "tablebank_both.pth": "https://drive.google.com/uc?id=1-vjfGRhF8kqvKwZPPFNwiTaOoonJlGgv",
     "icdar_19b2.pth": "https://drive.google.com/uc?id=1zmFbQ_3QToD22XHhKpnRnhBVOgKl-Lp7",
     "icdar_19b2_v2.pth": "https://drive.google.com/uc?id=1dmYzZP8eF6_EkkJagN6NJ9A7ReOy_ICY",
+    # from official cascadetabnet github demo notebook (https://github.com/DevashishPrasad/CascadeTabNet/blob/517933ccd82c3377ab35fc40f800d99d59571837/Demo/Cascade_TabNet_Demo.ipynb)
+    "cascadetabnet_epoch_36.pth": "https://drive.google.com/u/0/uc?id=1-QieHkR1Q7CXuBu4fp3rYrvDG9j26eFT",
 }
