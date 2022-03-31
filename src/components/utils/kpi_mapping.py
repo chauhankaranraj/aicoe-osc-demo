57748752bbca4ceb9ae1b235e8c527afba03409f
Karanraj Chauhan <kachau@redhat.com>
2022-03-31 17:13:31 +0000

    Add relevance model training notebook. Trains on 145 sample pdfs.
    Squashed commits
    - Update train nb to use model config and 145 pdfs from OSC team.
    - Update model parameters and dataset config
    - Clean up and rename notebook. WIP evaluation
    - Save model to s3
    - Update outputs
    
    Signed-off-by: Karanraj Chauhan <kachau@redhat.com>
    Co-authored-by: Shrey Anand <shanand@redhat.com>

--- src/components/utils/kpi_mapping.py
+++ src/components/utils/kpi_mapping.py
@@ -4,19 +4,19 @@ def get_kpi_mapping_category(df):
     """
     _KPI_MAPPING = {str(i[0]): i[1] for i in df[["kpi_id", "question"]].values}
     KPI_MAPPING = {(float(key)): value for key, value in _KPI_MAPPING.items()}
-    
+
     # Category where the answer to the question should originate from
     KPI_CATEGORY = {
     i[0]: [j.strip() for j in i[1].split(", ")]
     for i in df[["kpi_id", "kpi_category"]].values
     }
-    
+
     _KPI_MAPPING_MODEL = {
     str(i[0]): (i[1], [j.strip() for j in i[2].split(",")])
     for i in df[["kpi_id", "question", "sectors"]].values
     }
     KPI_MAPPING_MODEL = {(float(key)): value for key, value in _KPI_MAPPING_MODEL.items()}
-    
+
     return {"KPI_MAPPING": KPI_MAPPING,
             "KPI_CATEGORY": KPI_CATEGORY,
             "KPI_MAPPING_MODEL": KPI_MAPPING_MODEL}
